97b8f691b10797411fd10971da4e1572398f0fa5
Thanussian Sharvananthan <thanussian@gmail.com>
2024-05-08 15:26:45 -0400

    Finished a huge chunk of the decryption.
    Is it accurate? No. Still need to fix a lot of my code. It seems that
    something went wrong in one of the functions. It's a matter of seeing
    where the process went wrong.
    
    I'm gonna ride my bike.

--- python/solution.py
+++ python/solution.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 from string import ascii_uppercase
 
 def _generate_playfair_grid(keyword: str,
@@ -42,3 +42,91 @@ def _generate_playfair_grid(keyword: str,
                      for i in range(0, len(playfair_row), 5)]
     
     return playfair_grid
+
+def _generate_position_map(lst: list) -> Dict[str, tuple]:
+    """ Maps values of a 2D list to their respective postions.
+
+    Args:
+        lst (list): A non-empty 2D list.
+
+    Returns:
+        Dict[str, tuple]: A dictionary that maps values of the given 2D
+        list to tuple (x, y).
+    """
+    position_map = {}
+
+    for i, rows in enumerate(playfair_grid):
+        for j, value in enumerate(rows):
+            position_map[value] = (i, j)
+
+    return position_map
+
+def _generate_bigrams(message: str) -> List[str]:
+    """ Generate bigrams from a message encrypted by the Playfair cipher.
+
+    Args:
+        message (str): A given message, most likely one from the Playfair
+        cipher.
+
+    Returns:
+        List[str]: A list of two character bigrams. Note that repeated
+        characters have an "X" inserted in between.
+    """
+    for i in range(len(message)-1):
+        if message[i] == message[i+1]:
+            message = message[0:i+1] + "X" + message[i+1::]
+
+    if len(message) % 2 != 0:
+        message += "X"
+
+    bigrams = [message[i: i+2] for i in range(0, len(message), 2)]
+
+    return bigrams
+
+def decrypt_playfair_cipher(encrypted_message: str, keyword: str) -> str:
+    """ Decrypt a message encrypted by the Playfair cipher.
+
+    Args:
+        encrypted_message (str): An encrypted message.
+        keyword (str): A phrase that represents the first few unique
+        characters of a Playfair grid.
+
+    Returns:
+        str: A decrypted message.
+    """
+
+    playfair_grid = _generate_playfair_grid(keyword)
+    position_map = _generate_position_map(playfair_grid)
+    encrypted_bigrams = _generate_bigrams(encrypted_message)
+
+    decrypted_bigrams = []
+
+    for encrypted_bigram in encrypted_bigrams:
+        first_char, second_char = encrypted_bigram
+
+        first_char_row, first_char_col = position_map[first_char]
+        second_char_row, second_char_col = position_map[second_char]
+
+        if first_char_row == second_char_row:
+            first_char_decrypt = playfair_grid[first_char_row][(first_char_col - 1) % 5]
+            second_char_decrypt = playfair_grid[second_char_row][(second_char_col - 1) % 5]
+
+        elif first_char_col == second_char_col:
+            first_char_decrypt = playfair_grid[(first_char_row - 1) % 5][first_char_col]
+            second_char_decrypt = playfair_grid[(second_char_row - 1) % 5][second_char_col]
+        
+        else:
+            first_char_decrypt = playfair_grid[first_char_row][second_char_col]
+            second_char_decrypt = playfair_grid[second_char_row][first_char_col]
+
+        decrypted_bigram = first_char_decrypt + second_char_decrypt
+        decrypted_bigrams.append(decrypted_bigram)
+
+        decrypted_message = "".join(decrypted_bigrams)
+
+    return decrypted_message
+
+encrypted_message = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
+key = "SUPERSPY"
+playfair_grid = _generate_playfair_grid(encrypted_message)
+print(decrypt_playfair_cipher(encrypted_message, key))
